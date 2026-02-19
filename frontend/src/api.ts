81efd8b2913fb0539b179cb09f3ebf3c289767ac
Younes234 <yalahm01@student.bbk.ac.uk>
2026-02-19 17:57:12 +0000

    change api url

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:4000/api";
+//const API_BASE_URL = "http://localhost:4000/api";
+const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
+
 
 // --- Token helpers ---
 
